5a0f294cc3a4f873a98c37348b892c491336a5e3
mfolina <maria.theodora.folina@cern.ch>
2026-03-24 09:55:27 +0100

    fix: chlep upd

--- clhep.sh
+++ clhep.sh
@@ -8,10 +8,12 @@ build_requires:
 ---
 #!/bin/bash -e
 ##############################
+# shellcheck source=/dev/null
 . $(bits-include CMakeRecipe)
 ##############################
-MODULE_OPTIONS="--bin --lib --cmake"
+# SC2034 (warning): MODULE_OPTIONS appears unused. Verify use (or export if used externally)
+# MODULE_OPTIONS="--bin --lib --cmake"
 ##############################
 function Configure() {
-  cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+  cmake "$SOURCEDIR" -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"
 }
