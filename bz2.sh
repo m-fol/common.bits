36b5d80513fa09c41a6165ffa37a76622b5eb47b
mfolina <maria.theodora.folina@cern.ch>
2026-03-24 09:25:17 +0100

    fix: bz2 bash add

--- bz2.sh
+++ bz2.sh
@@ -9,19 +9,19 @@ prefer_system: "(?!slc5)"
 prefer_system_check: |
   printf "#include <bzlib.h>\n" | c++ -xc++ - -c -o /dev/null
 ---
-
+#!/bin/bash
 # Notice how this will build just the archive library.
-rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
-make ${JOBS:+-j $JOBS} CFLAGS="-O2 -fPIC -D_FILE_OFFSET_BITS=64" PREFIX=$INSTALLROOT install
-rm -rf $INSTALLROOT/bin/bzfgrep $INSTALLROOT/bin/bzegrep $INSTALLROOT/bin/bzcmp $INSTALLROOT/bin/bzless
-ln ./bzgrep $INSTALLROOT/bin/bzfgrep
-ln ./bzgrep $INSTALLROOT/bin/bzegrep 
-ln ./bzdiff $INSTALLROOT/bin/bzcmp
-ln ./bzmore $INSTALLROOT/bin/bzless
+rsync -a --delete --exclude '**/.git' --delete-excluded "$SOURCEDIR"/ ./
+make ${JOBS:+-j $JOBS} CFLAGS="-O2 -fPIC -D_FILE_OFFSET_BITS=64" PREFIX="$INSTALLROOT" install
+rm -rf "$INSTALLROOT"/bin/bzfgrep "$INSTALLROOT"/bin/bzegrep "$INSTALLROOT"/bin/bzcmp "$INSTALLROOT"/bin/bzless
+ln ./bzgrep "$INSTALLROOT"/bin/bzfgrep
+ln ./bzgrep "$INSTALLROOT"/bin/bzegrep 
+ln ./bzdiff "$INSTALLROOT"/bin/bzcmp
+ln ./bzmore "$INSTALLROOT"/bin/bzless
 
 # Not really needed. Just in case someone decides to build shared libraries
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-alibuild-generate-module --bin --lib > $MODULEFILE
+alibuild-generate-module --bin --lib > "$MODULEFILE"
